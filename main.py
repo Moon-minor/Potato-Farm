25cd4764246c53ba90108b301db3cf9606c96314
Moon-minor <dibochh@gmail.com>
2026-06-25 17:31:31 +0800

    farm(), save(), shop() function added Other minor functions added

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import time as t
+import sqlite3 as sql
 
-
-
+#initiation
+conn = sql.connect('farm.db')
 
 def menu():
-    print("Welcome to Potato Farm!!\n==================")
+    print("Welcome to Potato Farm!!\n=======================")
+    print("1.Farm\n2.Shop\n3.Exit")
+    choice = input("Enter Choice: ")
+    return choice
+
+def Farm():
+    pass
 #main:
 menu()
-print("hi")
